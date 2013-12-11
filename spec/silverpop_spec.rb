61664238839e5fc9a7c8e8b5affbaead04753c04
Ryan Resella <ryanresella@gmail.com>
2013-12-10 16:34:27 -0800

    Add spec for respond_to

--- spec/silverpop_spec.rb
+++ spec/silverpop_spec.rb
@@ -6,4 +6,10 @@ describe SilverPop do
       SilverPop.new.should be_a SilverPop::Client
     end
   end
+
+  describe ".respond_to?" do
+    it "should be true if method exists" do
+      SilverPop.respond_to?(:new, true).should be_true
+    end
+  end
 end
