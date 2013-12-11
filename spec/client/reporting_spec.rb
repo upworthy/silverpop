c061f39e1bf9d69ef4d535f863873ad15baea857
Ryan Resella <ryanresella@gmail.com>
2013-12-10 16:29:14 -0800

    Move the return body into a fixture

--- spec/client/reporting_spec.rb
+++ spec/client/reporting_spec.rb
@@ -7,9 +7,9 @@ describe SilverPop::Client::Reporting do
 
   describe ".raw_recipient_data_export" do
     it "should return true when passing a MAILING_ID" do
-      stub_request(:post, "https://api1.silverpop.com/XMLAPI?access_token=abc123").
+      stub_post("/XMLAPI?access_token=abc123").
         with(:body => "<Envelope><Body><RawRecipientDataExport><MAILING><MAILING_ID>1234</MAILING_ID></MAILING></RawRecipientDataExport></Body></Envelope>").
-          to_return(:status => 200, :body => "<Envelope><Body><RESULT><SUCCESS>TRUE</SUCCESS><MAILING><JOB_ID>72649</JOB_ID><FILE_PATH>15167_20041213100410_track.zip</FILE_PATH></MAILING></RESULT></Body></Envelope>", :headers => {'Content-type' => "text/xml"})
+          to_return(:status => 200, :body => fixture("reporting.xml"), :headers => {'Content-type' => "text/xml"})
 
 
       resp = @client.raw_recipient_data_export({MAILING_ID: 1234})
@@ -17,9 +17,9 @@ describe SilverPop::Client::Reporting do
     end
 
     it "should return true when passing a REPORT_ID" do
-      stub_request(:post, "https://api1.silverpop.com/XMLAPI?access_token=abc123").
+      stub_post("/XMLAPI?access_token=abc123").
         with(:body => "<Envelope><Body><RawRecipientDataExport><MAILING><REPORT_ID>5678</REPORT_ID></MAILING></RawRecipientDataExport></Body></Envelope>").
-          to_return(:status => 200, :body => "<Envelope><Body><RESULT><SUCCESS>TRUE</SUCCESS><MAILING><JOB_ID>72649</JOB_ID><FILE_PATH>15167_20041213100410_track.zip</FILE_PATH></MAILING></RESULT></Body></Envelope>", :headers => {'Content-type' => "text/xml"})
+          to_return(:status => 200, :body => fixture("reporting.xml"), :headers => {'Content-type' => "text/xml"})
 
 
       resp = @client.raw_recipient_data_export({REPORT_ID: 5678})
@@ -27,9 +27,9 @@ describe SilverPop::Client::Reporting do
     end
 
     it "should return true when passing a EVENT_DATE_START and EVENT_DATE_END" do
-      stub_request(:post, "https://api1.silverpop.com/XMLAPI?access_token=abc123").
+      stub_post("/XMLAPI?access_token=abc123").
          with(:body => "<Envelope><Body><RawRecipientDataExport><EVENT_DATE_START>1/1/2013</EVENT_DATE_START><EVENT_DATE_END>7/1/2013</EVENT_DATE_END></RawRecipientDataExport></Body></Envelope>").
-        to_return(:status => 200, :body => "<Envelope><Body><RESULT><SUCCESS>TRUE</SUCCESS><MAILING><JOB_ID>72649</JOB_ID><FILE_PATH>15167_20041213100410_track.zip</FILE_PATH></MAILING></RESULT></Body></Envelope>", :headers => {'Content-type' => "text/xml"})
+          to_return(:status => 200, :body => fixture("reporting.xml"), :headers => {'Content-type' => "text/xml"})
 
 
       resp = @client.raw_recipient_data_export({EVENT_DATE_START: "1/1/2013", EVENT_DATE_END: "7/1/2013"})
@@ -37,20 +37,18 @@ describe SilverPop::Client::Reporting do
     end
 
     it "should return true when passing options" do
-      stub_request(:post, "https://api1.silverpop.com/XMLAPI?access_token=abc123").
+      stub_post("/XMLAPI?access_token=abc123").
         with(:body => "<Envelope><Body><RawRecipientDataExport><MAILING><MAILING_ID>1234</MAILING_ID></MAILING><MOVE_TO_FTP/></RawRecipientDataExport></Body></Envelope>").
-          to_return(:status => 200, :body => "<Envelope><Body><RESULT><SUCCESS>TRUE</SUCCESS><MAILING><JOB_ID>72649</JOB_ID><FILE_PATH>15167_20041213100410_track.zip</FILE_PATH></MAILING></RESULT></Body></Envelope>", :headers => {'Content-type' => "text/xml"})
-
+          to_return(:status => 200, :body => fixture("reporting.xml"), :headers => {'Content-type' => "text/xml"})
 
       resp = @client.raw_recipient_data_export({MAILING_ID: 1234},{MOVE_TO_FTP: nil})
       resp.Envelope.Body.RESULT.SUCCESS.should eql "TRUE"
-
     end
 
     it "should return true when passing extra columns to export" do
-      stub_request(:post, "https://api1.silverpop.com/XMLAPI?access_token=abc123").
+      stub_post("/XMLAPI?access_token=abc123").
         with(:body => "<Envelope><Body><RawRecipientDataExport><MAILING><MAILING_ID>1234</MAILING_ID></MAILING><COLUMNS><COLUMN><NAME>CustomerID</NAME></COLUMN><COLUMN><NAME>Address</NAME></COLUMN></COLUMNS></RawRecipientDataExport></Body></Envelope>").
-          to_return(:status => 200, :body => "<Envelope><Body><RESULT><SUCCESS>TRUE</SUCCESS><MAILING><JOB_ID>72649</JOB_ID><FILE_PATH>15167_20041213100410_track.zip</FILE_PATH></MAILING></RESULT></Body></Envelope>", :headers => {'Content-type' => "text/xml"})
+          to_return(:status => 200, :body => fixture("reporting.xml"), :headers => {'Content-type' => "text/xml"})
 
       resp = @client.raw_recipient_data_export({MAILING_ID: 1234},{}, ["CustomerID", "Address"])
       resp.Envelope.Body.RESULT.SUCCESS.should eql "TRUE"
--- spec/helper.rb
+++ spec/helper.rb
@@ -7,3 +7,19 @@ end
 require 'silverpop'
 require 'rspec'
 require 'webmock/rspec'
+
+def stub_post(url)
+  stub_request(:post, silverpop_url(url))
+end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
+
+def silverpop_url(url)
+  "https://api1.silverpop.com#{url}"
+end
