9184dcbba4e3fab12763e20471dfd336a9499c24
Ryan Resella <ryanresella@gmail.com>
2015-01-29 15:44:44 -0800

    Rname to file.csv to make circleci happy

--- spec/client/user_spec.rb
+++ spec/client/user_spec.rb
@@ -29,7 +29,7 @@ describe SilverPop::Client::User do
     it 'returns the job_id of the relational table' do
       stub_post("/XMLAPI?access_token=abc123").
         with(:body => "<Envelope><Body><ExportTable><TABLE_NAME>59294</TABLE_NAME><EXPORT_FORMAT>CSV</EXPORT_FORMAT><ADD_TO_STORED_FILES/><DATE_START>07/25/2011 12:12:11</DATE_START><DATE_END>09/30/2011 14:14:11</DATE_END></ExportTable></Body></Envelope>").
-        to_return(:status => 200, :body => "<Envelope><Body><RESULT><SUCCESS>TRUE</SUCCESS><JOB_ID>499600</JOB_ID><FILE_PATH>/download/Purchases – Oct 11 2011 03-58- 17PM.CSV</FILE_PATH></RESULT></Body></Envelope>", :headers => {'Content-type' => "text/xml"})
+        to_return(:status => 200, :body => "<Envelope><Body><RESULT><SUCCESS>TRUE</SUCCESS><JOB_ID>499600</JOB_ID><FILE_PATH>file.CSV</FILE_PATH></RESULT></Body></Envelope>", :headers => {'Content-type' => "text/xml"})
 
       resp = @client.export_table('59294','CSV',{ADD_TO_STORED_FILES: nil,  DATE_START: "07/25/2011 12:12:11", DATE_END:"09/30/2011 14:14:11"})
       resp.Envelope.Body.RESULT.JOB_ID.should eql "499600"
