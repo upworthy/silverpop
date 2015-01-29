8e152d3acc73e71ab80ba5dd8d75fd93762e9973
Ryan Resella <ryanresella@gmail.com>
2015-01-29 15:39:25 -0800

    Add the function to export a relational table

--- lib/client/user.rb
+++ lib/client/user.rb
@@ -40,6 +40,35 @@ module SilverPop
       end
 
     end
+
+    # ExportTable - This interface supports programmatically exporting Relational Table data from Engage into a CSV file, which Engage uploads to the FTP account or to the Stored Files directory associated with the session.
+    #
+    # @param table_id [String] Optional parameter to specify the ID of the Relational Table you are exporting. Either TABLE_NAME or TABLE_ID is required.
+    # @param export_format [String] Specifies the format (file type) for the exported data, CSV, TAB, PIPE.
+    # @param options [Hash] Optional parameters to send
+    # @param export_colums [Array] XML node used to request specific custom database columns to export for each contact.
+    # @return [Mash] Mashify body from the API call
+    # @example Export Table 12345 for 1/1/2014 to 1/2/2014
+    #   s = SilverPop.new({access_token: "abc123", url: "https://api1.silverpop.com"})
+    #   s.export_table('12345', 'CSV', {DATE_START: "1/1/2014", DATE_END:"1/2/2014"})
+    def export_table(table_id, export_format, options={})
+      builder = Builder::XmlMarkup.new
+      xml = builder.Envelope {
+        builder.Body {
+          builder.ExportTable {
+            builder.TABLE_NAME table_id
+            builder.EXPORT_FORMAT export_format
+            unless options.empty?
+              options.each do |o|
+                builder.tag! o[0], o[1]
+              end
+            end
+            }
+          }
+      }
+      post(xml)
+    end
+
   end
 end
 
